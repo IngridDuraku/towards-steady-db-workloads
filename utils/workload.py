d13fe2c9222ac68d067b1774523d976d5f78ff1e
IngridDuraku <ingrid.duraku@fti.edu.al>
2025-11-24 02:03:12 +0100

    query load

--- utils/workload.py
+++ utils/workload.py
@@ -9,14 +9,14 @@ def estimate_query_load(query, ref_values):
     weights = {
         "bytes_scanned": 0.8,
         "result_size": 0.5,
-        "write_volume": 1.2,
+        "write_volume": 0.8,
         "cpu_time": 1.5,
     }
 
     bs_factor = 0 if ref_values["bytes_scanned"] == 0 else weights["bytes_scanned"] * query.bytes_scanned / ref_values["bytes_scanned"]
     rs_factor = 0 if ref_values["result_size"] == 0 else weights["result_size"] * query.result_size / ref_values["result_size"]
     wv_factor = 0 if ref_values["write_volume"] == 0 else weights["write_volume"] * query.write_volume / ref_values["write_volume"]
-    cpu_factor = 0 if ref_values["cpu_time"] == 0 else weights["cpu_time"] * query.cpu_time / ref_values["cpu_time"]
+    cpu_factor = 0 if ref_values["cpu_time"] == 0 else weights["cpu_time"] * query.cpu_time # / ref_values["cpu_time"]
 
     load = bs_factor + rs_factor + wv_factor + cpu_factor
 
